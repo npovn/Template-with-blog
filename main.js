2e1f90e4c4352b1ed45e32c196b0e9c5c3303347
DangGiaChi <danggiachi@gmail.com>
2022-04-02 22:06:58 +0700

    Header for aboutus.html

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ window.addEventListener("scroll", function(){
     Parallax.style.transform = "translateY(" + offsetY * 0.7 + "px)";
 })
 
+window.addEventListener("scroll", function(){
+    const Parallax = document.querySelector(".parallax1");
+    let offsetY = this.window.scrollY;
+    Parallax.style.transform = "translateY(" + offsetY + "px)";
+})
+
