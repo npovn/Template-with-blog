374f3744c80de3e7841d897a9f622440955fe6b8
DangGiaChi <danggiachi@gmail.com>
2022-05-26 22:59:28 +0700

    Website basically complete

--- _site/header.js
+++ _site/header.js
@@ -2,6 +2,7 @@ const btncontainer = document.querySelector(".burger_container");
 const btnmenu = document.querySelector(".burger_menu");
 let menuOpen = false;
 btncontainer.addEventListener("click", function(){
+    console.log("Clicked");
     if(!menuOpen){
         btncontainer.classList.add("open");
         btnmenu.classList.add("open");
